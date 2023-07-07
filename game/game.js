d924ba3aa695463e4d677dcd61abfb5f52721826
benwilliamgraham <benwilliamgraham@gmail.com>
2023-07-07 11:32:26 -0700

    Resizing

--- game/game.js
+++ game/game.js
@@ -1,9 +1,34 @@
 "use strict";
 
+// Setup document
+document.body.style.margin = "0";
+document.body.style.padding = "0";
+document.body.style.overflow = "hidden";
+
 const canvas = document.createElement("canvas");
-canvas.width = 800;
-canvas.height = 600;
 document.body.appendChild(canvas);
+
+// Setup canvas
+const mapWidth = 40;
+const mapHeight = 30;
+let tileSize = 1;
+
+function resize() {
+  const aspect = mapWidth / mapHeight;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  if (width / height > aspect) {
+    canvas.width = height * aspect;
+    canvas.height = height;
+    tileSize = height / mapHeight;
+  } else {
+    canvas.width = width;
+    canvas.height = width / aspect;
+    tileSize = width / mapWidth;
+  }
+}
+window.addEventListener("resize", resize);
+resize();
 
 const context = canvas.getContext("2d");
 
@@ -22,7 +47,6 @@ function draw(time) {
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = "black";
   context.fillRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "red";
   context.drawImage(images.demo, x, 0, 100, 100);
   x += 10;
   if (x > canvas.width) {
